7f30b4b8dd56bdf9f810e43faa69bfd927a3e067
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2016-12-28 21:04:11 +0000

    Adding definitions of mountains. Should not stay here!

--- mynearestmountain/mynearestmountain/app.js
+++ mynearestmountain/mynearestmountain/app.js
@@ -19,6 +19,11 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'));
 
+var mountains = [
+	{ name: "Ben Nevis", location: [56, 47, 49, 'N', 5, 0, 13, 'W']},
+	{ name: "Scafell Pike", location: [54, 27, 15.2, 'N', 3, 12, 41.5 'W']},
+	{ name: "Snowdon", location: [53, 4, 6.59, 'N', 4, 4, 34.43 'W']}
+]
 
 app.get('/', function (req, res) {
   res.render('index', { title : 'Home' } );
