9dc66183d15e6cb1fd611d33108cd40aeee95fbe
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2016-12-30 19:10:00 +0000

    Plotting route from current location to a hardcoded mountain

--- mynearestmountain/mynearestmountain/public/viewmodels/index.js
+++ mynearestmountain/mynearestmountain/public/viewmodels/index.js
@@ -4,29 +4,56 @@ var mountains = [
 	{ id: 3, name: "Snowdon", location: {lat: 53.068497, lng: -4.076231}}
 ];
 	
-var initMap = function(location) {
+var initMap = function(destinationLocation) {
 	if(!google)
 		return;
-	
+
 	var map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 11,
 	  center: location
 	});
-	var marker = new google.maps.Marker({
-	  position: location,
-	  map: map
-	});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+	        var currentPosition = {
+	          lat: position.coords.latitude,
+	          lng: position.coords.longitude
+	        };
+
+	        var targetPosition = mountains[2].location;
+
+	        var directionsService = new google.maps.DirectionsService();
+	        var directionsDisplay = new google.maps.DirectionsRenderer();
+	        directionsDisplay.setMap(map);
+
+	        directionsService.route({
+	        	origin: currentPosition,
+			    destination: targetPosition,
+			    travelMode: 'DRIVING'
+	        }, function(result, status) {
+				if (status == 'OK') {
+				    directionsDisplay.setDirections(result);
+				}
+	        });
+      	}, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+  }
+
 }
 
-var model = function() {
-	this.mountains = ko.observableArray(mountains);
-
-	this.selectedMountain = ko.observable();
-	this.selectedMountain.subscribe(function(val) {
-		var mountain = mountains.filter(function(x) { return x.id === val; })[0];
-		initMap(mountain.location);
-	});
-}
+var model = function() {}
 
 var viewModel = new model();
 ko.applyBindings(viewModel);
