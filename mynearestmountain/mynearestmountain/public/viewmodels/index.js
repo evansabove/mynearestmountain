fe428517e4bf15c7b70ef45e6d058be2f7771006
Andy Evans <andy.evans@sword-activerisk.com>
2016-12-30 16:08:24 +0000

    Refactoring mountain list into google maps api scope

--- mynearestmountain/mynearestmountain/public/viewmodels/index.js
+++ mynearestmountain/mynearestmountain/public/viewmodels/index.js
@@ -1,11 +1,21 @@
+var mountains = [
+	{ name: "Ben Nevis", location: {lat: 56.796944, lng: -5.003611}},
+	{ name: "Scafell Pike", location: {lat: 54.454222, lng: -3.211528}},
+	{ name: "Snowdon", location: {lat: 53.068497, lng: -4.076231}}
+];
+	
+var initMap = function() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 11,
+	  center: mountains[1].location
+	});
+	var marker = new google.maps.Marker({
+	  position: mountains[1].location,
+	  map: map
+	});
+}
+
 function model() {
-	
-	var mountains = [
-		{ name: "Ben Nevis", location: {lat: 56.796944, lng: -5.003611}},
-		{ name: "Scafell Pike", location: {lat: 54.454222, lng: -3.211528}},
-		{ name: "Snowdon", location: {lat: 53.068497, lng: -4.076231}}
-	]
-	
 	this.mountains = mountains.map(function(mountain) { return mountain.name; });
 }
 
