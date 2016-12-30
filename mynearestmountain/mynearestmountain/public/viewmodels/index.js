43284972093ee5f4f7076c0ecf8df1bbd2d72d2e
Andy Evans <andy.evans@sword-activerisk.com>
2016-12-30 16:04:25 +0000

    Transforming coordinates to decimal format

--- mynearestmountain/mynearestmountain/public/viewmodels/index.js
+++ mynearestmountain/mynearestmountain/public/viewmodels/index.js
@@ -1,9 +1,9 @@
 function model() {
 	
 	var mountains = [
-		{ name: "Ben Nevis", location: [56, 47, 49, 'N', 5, 0, 13, 'W']},
-		{ name: "Scafell Pike", location: [54, 27, 15.2, 'N', 3, 12, 41.5, 'W']},
-		{ name: "Snowdon", location: [53, 4, 6.59, 'N', 4, 4, 34.43, 'W']}
+		{ name: "Ben Nevis", location: {lat: 56.796944, lng: -5.003611}},
+		{ name: "Scafell Pike", location: {lat: 54.454222, lng: -3.211528}},
+		{ name: "Snowdon", location: {lat: 53.068497, lng: -4.076231}}
 	]
 	
 	this.mountains = mountains.map(function(mountain) { return mountain.name; });
