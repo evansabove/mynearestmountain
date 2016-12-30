5ebe7a9d4a666a604f91eca49fb9977940f0c187
Andy Evans <andy.evans@sword-activerisk.com>
2016-12-30 14:48:37 +0000

    Map is working!

--- mynearestmountain/mynearestmountain/app.js
+++ mynearestmountain/mynearestmountain/app.js
@@ -19,10 +19,8 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'));
 
-
-
 app.get('/', function (req, res) {
-  res.render('index', { title : 'Home' } );
+  res.render('index')
 });
 
 app.listen(3000);
