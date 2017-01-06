88e47880eddefb85869db6c36bb54f8fa0642dd2
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-06 22:09:50 +0000

    Attempt at a favicon, not working, plus logging

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express')
-	, path = require('path');
+	, path = require('path')
+	, logger = require('morgan')
+	, favicon = require('serve-favicon');
 
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use(logger('combined'))
+app.use('/favicon.ico', express.static('/public/images/favicon.ico'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
