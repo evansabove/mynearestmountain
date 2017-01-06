7911efecce862fb2362d62ef8806f28cbc1afb4e
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-06 20:53:29 +0000

    Correcting previous commit

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var express = require('express')
-  , nib = require('nib');
+var express = require('express');
 
 var app = express();
 
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
-}
-app.set('views', __dirname + '/views')
-app.set('view engine', 'pug')
-app.use(express.logger('dev'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
