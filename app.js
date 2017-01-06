0c00223c64735a840e04be0c92e7f5972ce53322
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-06 21:21:56 +0000

    Removing the dependency on pug

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var express = require('express');
+var express = require('express')
+	, path = require('path');
 
 var app = express();
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.listen(3000);
